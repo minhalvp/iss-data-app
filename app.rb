ae6acad80751b5f2831df64247330c390cb24934
minhalvp <minhaalvp@gmail.com>
2024-05-13 23:27:54 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# myapp.rb
+require 'sinatra'
+require_relative 'open_notify'
+
+# Allow our templates in views/ to end in `.html.erb`
+Tilt.register Tilt::ERBTemplate, 'html.erb'
+
+set :layout, 'views/layout.html.erb'
+
+get '/' do
+  erb :index
+end
+
+get '/position' do
+  iss_now = OpenNotify.iss_now
+
+  erb :position, locals: { data: iss_now }
+end
+
+get '/astros' do
+  astros = OpenNotify.astros
+
+  # content_type :json Commented out because we are not returning JSON
+  astros.to_json
+  erb :astros, locals: { data: astros}
+end
